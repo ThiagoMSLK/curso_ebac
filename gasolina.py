248c3790cd760919f64df5f8a1bb610cd97bcdf0
ThiagoMSLK <thiagolk2704@gmail.com>
2024-09-11 00:56:19 +0000

    Atualizando o codigo do grafico

--- gasolina.py
+++ gasolina.py
@@ -7,7 +7,7 @@ df = pd.read_csv('gasolina.csv')
 with sns.axes_style('darkgrid'):
   grafico = sns.lineplot(data=df, x='dia', y='venda', color='blue', errorbar=('ci', True))
   sns.regplot(data=df, x='dia', y='venda', scatter=False, color='red', ax=grafico)
-  grafico.set(title='Preço da Gasolina dos Ultimos 10 Dias', xlabel='Dias', ylabel='Vendas')
+  grafico.set(title='Preço da Gasolina dos Ultimos 10 Dias', xlabel='Dias', ylabel='Valor Vendas')
   plt.tight_layout()
   plt.show()
   plt.savefig('gasolina.png')
